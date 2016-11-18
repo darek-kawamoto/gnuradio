fa097056bf8861a704cca4f7ea866a77589132b1
Darek Kawamoto <darek@he360.com>
2016-11-18 14:57:31 -0500

    Incorporating selected portions of bastibl's updates from #797 to see if it alleviates some pmt problems.

--- gnuradio-runtime/lib/block.cc
+++ gnuradio-runtime/lib/block.cc
@@ -708,7 +708,7 @@ namespace gr {
     pmt::pmt_t op = pmt::car(msg);
     if(pmt::eqv(op, pmt::mp("done"))){
         d_finished = pmt::to_long(pmt::cdr(msg));
-        global_block_registry.notify_blk(alias());
+        //global_block_registry.notify_blk(alias());
     } else {
         std::cout << "WARNING: bad message op on system port!\n";
         pmt::print(msg);
@@ -750,7 +750,8 @@ namespace gr {
   bool
   block::finished()
   {
-    if((detail()->ninputs() != 0) || (detail()->noutputs() != 0))
+    //if((detail()->ninputs() != 0) || (detail()->noutputs() != 0))
+    if(detail()->ninputs() != 0)
       return false;
     else
       return d_finished;
